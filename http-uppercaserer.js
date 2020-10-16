5062f7e2f04770ce93d1d1cb4cccc9463e97f9d8
pefbrute <pefbrute@gmail.com>
2020-10-16 19:28:50 +0300

    +Finished the 12th task

--- http-uppercaserer.js
+++ http-uppercaserer.js
@@ -1,12 +1,29 @@
 const fs = require("fs");
 const http = require("http");
+const map = require("through2-map");
 
-let port = process.argv[2],
-  path = process.argv[3];
-
+let port = process.argv[2];
 const server = http
   .createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/plain'})
-    fs.createReadStream(path).pipe(res);
+    res.writeHead(200, { "content-type": "text/plain" });
+    // let readStream = fs.createWriteStream();
+    if (req.method === "POST") {
+      let body = "";
+      req.pipe(map((chunk) => {
+          return chunk.toString().toUpperCase();
+      })).pipe(res);
+    //   req.on("data", (chunk) => {
+    //     console.log(chunk.toString().toUpperCase());
+    //     body += chunk.toString().toUpperCase(); // convert Buffer to string
+    //     // req.pipe(res);
+    //   });
+      //   req.on("end", () => {
+    //   console.log(body);
+    //   res.end(body)
+    //   res.end("ok");
+      //   });
+    } else {
+        res.end("send me a POST\n")
+    }
   })
   .listen(port);
