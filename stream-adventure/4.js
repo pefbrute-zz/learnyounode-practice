6d2cdf13928eb99b2603f290b066147dd4890de8
pefbrute <pefbrute@gmail.com>
2020-10-17 18:02:08 +0300

    +Restructured the project and started stream-adventure

---
+++ stream-adventure/4.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const { Readable } = require('stream');
+const arguments = process.argv;
+const content = arguments[2];
+
+class MyStream extends Readable {
+    _read(size){
+    }
+}
+
+const stream = new MyStream(content);
+stream.push(content);
+stream.pipe(process.stdout);
