e84a4d8077c35725623cf13212c823adaf08ee45
pefbrute <pefbrute@gmail.com>
2020-10-18 19:55:14 +0300

    Solved the 10th task

--- stream-adventure/http-server.js
+++ stream-adventure/http-server.js
@@ -1,11 +1,6 @@
 const http = require("http");
 const through = require("through2");
 const server = http.createServer((req, res) => {
-  //   if (req.method === "POST") {
-  //     req.pipe(fs.createWriteStream("post.txt"));
-  //   }
-  //   res.end("beep boop\n");
-
    req.pipe(through(write, end)).pipe(res);
 });
 server.listen(process.argv[2]);
